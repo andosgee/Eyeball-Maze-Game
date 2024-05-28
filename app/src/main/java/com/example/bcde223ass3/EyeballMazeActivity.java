df6ae3f3d41addf434ef2a1c10433d0f81a5d4fa
Andy <66146370+andosgee@users.noreply.github.com>
2024-05-28 21:36:41 +1200

    Initial Layout
    Layout for must haves are completed

--- app/src/main/java/com/example/bcde223ass3/EyeballMazeActivity.java
+++ app/src/main/java/com/example/bcde223ass3/EyeballMazeActivity.java
@@ -1,10 +1,15 @@
 package com.example.bcde223ass3;
 
+// Import the model
+import com.example.bcde223ass3.model.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-// Import the model
-import com.example.bcde223ass3.model.*;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.util.Log;
 
 public class EyeballMazeActivity extends AppCompatActivity {
 
@@ -13,7 +18,14 @@ public class EyeballMazeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Game game = new Game();
+    }
 
+    private void createNewGame(int width, int height){
+        Game game = new Game();
+        game.addLevel(width, height);
+        int levelHeight = game.getLevelHeight();
+        int levelWidth = game.getLevelWidth();
+        String message = "Level Created. Height: " + levelHeight + "Width: " + levelWidth;
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
